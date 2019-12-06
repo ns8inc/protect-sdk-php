f2c2ca00993040c018fa5a0d489d127f72119a70
Chris <chris@hnas-MacBook-Pro.local>
2019-12-06 14:04:34 -0800

    Adding simple Exception file so code makes sense

---
+++ src/Config/Exceptions/Json.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace NS8\ProtectSDK\Config\Exceptions;
+
+use Exception;
+
+/**
+ * HTTP Exception used for NS8 HTTP Exceptions being triggered
+ **/
+class Json extends Exception
+{
+}
