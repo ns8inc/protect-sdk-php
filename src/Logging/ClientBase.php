8162adcd7ea84e270e6edfe70b9a4704a073847e
Chris <chris@hnas-MacBook-Pro.local>
2019-12-09 09:42:25 -0800

    Adding abstract class for Logging functionality

---
+++ src/Logging/ClientBase.php
@@ -0,0 +1,53 @@
+<?php
+
+declare(strict_types=1);
+
+namespace NS8\ProtectSDK\Logging;
+
+/**
+ * HTTP/Rest client that allows the NS8 SDK to communicate with NS8 services
+ */
+abstract class ClientBase
+{
+    /**
+     * @param string  $message  The message we intend to log
+     * @param mixed[] $data     Data associated with the error if any is present
+     * @param string  $function The function name associated with the error.
+     *
+     * @return void
+     */
+    abstract public function error(string $message, ?array $data = null, string $function = 'Unknown') : void;
+
+    /**
+     * Logs a message classified as a debugging line
+     *
+     * @param string  $message  The message we intend to log
+     * @param mixed[] $data     Data associated with the error if any is present
+     * @param string  $function The function name associated with the error.
+     *
+     * @return void
+     */
+    abstract public function debug(string $message, ?array $data = null, string $function = 'Unknown') : void;
+
+    /**
+     * Logs a message classified as a warning
+     *
+     * @param string  $message  The message we intend to log
+     * @param mixed[] $data     Data associated with the error if any is present
+     * @param string  $function The function name associated with the error.
+     *
+     * @return void
+     */
+    abstract public function warn(string $message, ?array $data = null, string $function = 'Unknown') : void;
+
+    /**
+     * Logs a message classified as information for developers/administrators
+     *
+     * @param string  $message  The message we intend to log
+     * @param mixed[] $data     Data associated with the error if any is present
+     * @param string  $function The function name associated with the error.
+     *
+     * @return void
+     */
+    abstract public function info(string $message, ?array $data = null, string $function = 'Unknown') : void;
+}
