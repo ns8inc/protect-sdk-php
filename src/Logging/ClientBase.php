9f32914a153bd865a87299d4be0e2a67fdf69ed2
Chris <chris@hnas-MacBook-Pro.local>
2019-12-09 14:02:01 -0800

    Updating Abstract Logging class to permit more dev-friendly usage

--- src/Logging/ClientBase.php
+++ src/Logging/ClientBase.php
@@ -10,44 +10,41 @@ namespace NS8\ProtectSDK\Logging;
 abstract class ClientBase
 {
     /**
-     * @param string  $message  The message we intend to log
-     * @param mixed[] $data     Data associated with the error if any is present
-     * @param string  $function The function name associated with the error.
+     * @param string    $message   The message we intend to log
+     * @param Exception $exception Exception object associated with the error.
+     * @param mixed[]   $data      Data associated with the error if any is present
      *
      * @return void
      */
-    abstract public function error(string $message, ?array $data = null, string $function = 'Unknown') : void;
+    abstract public function error(string $message, ?Exception $exception = null, ?array $data = null) : void;
 
     /**
      * Logs a message classified as a debugging line
      *
-     * @param string  $message  The message we intend to log
-     * @param mixed[] $data     Data associated with the error if any is present
-     * @param string  $function The function name associated with the error.
+     * @param string  $message The message we intend to log
+     * @param mixed[] $data    Data associated with the error if any is present
      *
      * @return void
      */
-    abstract public function debug(string $message, ?array $data = null, string $function = 'Unknown') : void;
+    abstract public function debug(string $message, ?array $data = null) : void;
 
     /**
      * Logs a message classified as a warning
      *
-     * @param string  $message  The message we intend to log
-     * @param mixed[] $data     Data associated with the error if any is present
-     * @param string  $function The function name associated with the error.
+     * @param string  $message The message we intend to log
+     * @param mixed[] $data    Data associated with the error if any is present
      *
      * @return void
      */
-    abstract public function warn(string $message, ?array $data = null, string $function = 'Unknown') : void;
+    abstract public function warn(string $message, ?array $data = null) : void;
 
     /**
      * Logs a message classified as information for developers/administrators
      *
-     * @param string  $message  The message we intend to log
-     * @param mixed[] $data     Data associated with the error if any is present
-     * @param string  $function The function name associated with the error.
+     * @param string  $message The message we intend to log
+     * @param mixed[] $data    Data associated with the error if any is present
      *
      * @return void
      */
-    abstract public function info(string $message, ?array $data = null, string $function = 'Unknown') : void;
+    abstract public function info(string $message, ?array $data = null) : void;
 }
