3cd99c7c4f37edd9cfa76809af4a27b865fd69d8
Chris <chris@hnas-MacBook-Pro.local>
2019-12-10 15:45:50 -0800

    Updating ENV exception info

---
+++ src/Config/Exceptions/Environment.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace NS8\ProtectSDK\Config\Exceptions;
+
+use Exception;
+
+/**
+ * Environment exception triggered when an exception occurs
+ * regarding the initialization or use of an environment incorrectly.
+ **/
+class Environment extends Exception
+{
+}
