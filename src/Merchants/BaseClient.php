225edcf872c58233e1d870001c7cfbdd2ba6c21b
Dave Hensley <dave.hensley@ns8.com>
2020-03-06 00:11:16 -0500

    Add Merchant client. CH-17154

---
+++ src/Merchants/BaseClient.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace NS8\ProtectSDK\Merchants;
+
+use stdClass;
+
+/**
+ * Base class for working with the NS8 Template Service
+ */
+abstract class BaseClient
+{
+    /**
+     * The template endpoint.
+     */
+    protected const CURRENT_MERCHANT_ENDPOINT = '/merchant/current';
+
+    /**
+     * Get the current merchant.
+     *
+     * @return stdClass The current merchant
+     */
+    abstract public static function getCurrent() : stdClass;
+}
