e15015502f34f4592f799ac61a9065eea133b9eb
Dave Hensley <dave.hensley@ns8.com>
2020-05-28 10:18:44 -0400

    Cache the TrueStats script in a temporary file for 24 hours. CH-28965

--- src/Analytics/Client.php
+++ src/Analytics/Client.php
@@ -5,7 +5,15 @@ declare(strict_types=1);
 namespace NS8\ProtectSDK\Analytics;
 
 use NS8\ProtectSDK\Http\Client as HttpClient;
+use function file_exists;
+use function file_get_contents;
+use function file_put_contents;
+use function filemtime;
+use function is_string;
 use function json_decode;
+use function sprintf;
+use function sys_get_temp_dir;
+use function time;
 
 /**
  * Class for dictating general NS8 Analytics components
@@ -16,6 +24,16 @@ class Client extends BaseClient
      * POST route for fetching TrueStats script from NS8
      */
     public const TRUE_STATS_ROUTE = '/init/script';
+
+    /**
+     * The temporary file used for caching the TrueStats script.
+     */
+    protected const TRUE_STATS_CACHE_FILE = 'ns8-truestats.json';
+
+    /**
+     * The TrueStats script gets cached for 1 day
+     */
+    protected const TRUE_STATS_CACHE_TTL = 86400;
 
     /**
      * HTTP Client used to make API requests
@@ -41,15 +59,26 @@ class Client extends BaseClient
      */
     public static function getTrueStatsScript() : string
     {
-        $script = self::getHttpClient()->sendNonObjectRequest(self::getTrueStatsRoute());
+        $cachedScript = self::getScriptFromCache();
 
-        return json_decode($script) ?? '';
+        if (isset($cachedScript)) {
+            return $cachedScript;
+        }
+
+        $script        = self::getHttpClient()->sendNonObjectRequest(self::getTrueStatsRoute());
+        $decodedScript = json_decode($script) ?? '';
+
+        if ($decodedScript !== '') {
+            self::saveScriptToCache($decodedScript);
+        }
+
+        return $decodedScript;
     }
 
     /**
      * Returns the HTTP client to be used for making API requests
      *
-     * @return HttpClient the client to be used
+     * @return HttpClient The client to be used
      */
     public static function getHttpClient() : HttpClient
     {
@@ -68,5 +97,45 @@ class Client extends BaseClient
     public static function setHttpClient(HttpClient $httpClient) : void
     {
         self::$httpClient = $httpClient;
+    }
+
+    /**
+     * Gets the full path to the cache file for the TrueStats script (platform independent)
+     *
+     * @return string The full path
+     */
+    protected static function getFullPathToScriptCacheFile() : string
+    {
+        return sprintf('%s/%s', sys_get_temp_dir(), self::TRUE_STATS_CACHE_FILE);
+    }
+
+    /**
+     * Gets the script from the cache (if available)
+     *
+     * @return string|null The cached script
+     */
+    protected static function getScriptFromCache() : ?string
+    {
+        $file = self::getFullPathToScriptCacheFile();
+
+        if (! file_exists($file) || filemtime($file) < time() - self::TRUE_STATS_CACHE_TTL) {
+            return null;
+        }
+
+        $script = file_get_contents($file);
+
+        return is_string($script) && $script !== '' ? $script : null;
+    }
+
+    /**
+     * Saves the script to the cache
+     *
+     * @param string $script The script
+     *
+     * @return void
+     */
+    protected static function saveScriptToCache(string $script) : void
+    {
+        file_put_contents(self::getFullPathToScriptCacheFile(), $script);
     }
 }
